3b4afff1b142fee63e438cb4ca1d86800969dc05
AmitKumarSahu <amitsahu6673@gmail.com>
2018-04-14 19:54:19 +0530

    example

--- project/urls.py
+++ project/urls.py
@@ -2,16 +2,15 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from welcome.views import index, health
+from welcome.views import index
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$', index),
-    url(r'^health$', health),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('welcome.urls')),
+
 ]
 
 if settings.DEBUG:
